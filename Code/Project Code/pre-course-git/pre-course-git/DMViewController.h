b07a44c007fd2ac2427ef16f7007b77d116b67ea
howiebarker <howiebarker7@gmail.com>
2014-05-03 17:23:03 -0600

    Initial Commit

---
+++ Code/Project Code/pre-course-git/pre-course-git/DMViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMViewController.h
+//  pre-course-git
+//
+//  Created by Howie Barker on 5/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMViewController : UIViewController
+
+@end
---
+++ Code/Project Code/pre-course-git/pre-course-git/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  pre-course-git
+//
+//  Created by Howie Barker on 5/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DMAppDelegate class]));
+    }
+}
