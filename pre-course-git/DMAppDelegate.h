fc50b86d990b8b3da0744ebc6bc2e82c3f4d7e54
howiebarker <howiebarker7@gmail.com>
2014-05-06 12:58:22 -0600

    pre course with colt

---
+++ pre-course-git/DMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DMAppDelegate.h
+//  pre-course-git
+//
+//  Created by Howie Barker on 5/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ pre-course-git/DMViewController.m
@@ -0,0 +1,29 @@
+//
+//  DMViewController.m
+//  pre-course-git
+//
+//  Created by Howie Barker on 5/3/14.
+//
+//
+
+#import "DMViewController.h"
+
+@interface DMViewController ()
+
+@end
+
+@implementation DMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
